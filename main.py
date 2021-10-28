f63ba0a34060b875f0a30e72b68fe9e7740bcd73
Memfis <memfis500@gmail.com>
2021-10-28 15:48:46 -0500

    users_show_all

--- main.py
+++ main.py
@@ -116,12 +116,27 @@ def login():
 @app.get(
     path="/users",
     response_model=List[User],
-    status_code=status.HTTP_201_CREATED,
+    status_code=status.HTTP_200_OK,
     summary="Show all users",
     tags=["Users"]
 )
 def show_all_users():
-    pass
+    """
+    This path operation shows all users in the app
+
+    Parameters:
+        -
+
+    Returns a json list with all users in the app, with the followings keys:
+        - user_id: UUID
+        - email: Emailstr
+        - first_name: str
+        - last_name: str
+        - birth_date: datetime
+    """
+    with open("users.json", "r", encoding="utf-8") as f:
+        results = json  .loads(f.read())
+        return results
 
 ### Show a user
 @app.get(
